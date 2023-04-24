4523cedbcc805b6dcb51b880c6d8976862ab9dd8
Ananth Prayaga <ananthp@uchicago.edu>
2023-04-24 02:39:39 +0000

    Added all directors, eldest and youngest

--- app/controllers/director_controller.rb
+++ app/controllers/director_controller.rb
@@ -1,11 +1,22 @@
 class DirectorController < ApplicationController
+
   def youngest_director
+    @youngest_director = Director.where.not({:dob => nil}).order({:dob => :desc}).at(0)
     render({ :template => "director_templates/youngestdirector.html.erb"})
+  end
+
+  def eldest_director
+    @eldest_director = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({ :template => "director_templates/eldestdirector.html.erb"})
   end
 
   def index
     @list_of_directors = Director.all
     render({:template => "director_templates/index.html.erb"})
+  end
+
+  def director_details
+    render({:template => "director_templates/directordetails.html.erb"})
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
-  get("/directors/", { :controller => "director", :action => "index"})
+  get("/directors", { :controller => "director", :action => "index"})
   get("/directors/youngest", { :controller => "director", :action => "youngest_director"})
+  get("/directors/eldest", { :controller => "director", :action => "eldest_director"})
+  get("/directors/:id", { :controller => "director", :action => "director_details"})
 end
